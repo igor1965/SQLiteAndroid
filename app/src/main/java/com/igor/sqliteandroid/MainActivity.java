79e820cf2d3de58ad065864f2b4beafb8aee22d8
igor1965 <sreznikov_igor@hotmail.com>
2018-01-16 05:54:05 -0500

    commit sqlite

--- app/src/main/java/com/igor/sqliteandroid/MainActivity.java
+++ app/src/main/java/com/igor/sqliteandroid/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd,btnRead,btnUpdate,btnDelete;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
